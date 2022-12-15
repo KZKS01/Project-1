a4340576bb438a899acd77ddb263c7e346485ed2
KZKS01 <kath.scholl@gmail.com>
2022-12-14 21:18:31 -0500

    WIP: added CSS & API key

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// alert(`Ready!`);
+$(document).ready(function() { 
+  
+    const $input1 = $("#box1");
+
+
+    let gifData;
+    
+    $(".btn").on("click", function(evt){
+
+        // alert("The button is clicked!");
+        evt.preventDefault(); // prevents a page refresh on a 'click' event 
+        // console.log($input1.val());
+        var userInput1 = $input1.val();
+        getGif();
+
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/trending?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+      }).then(
+        (data) => {
+          console.log(data.data[0].images.downsized_medium.url);
+          
+          
+        },
+        
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+    
+      $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+      }).then(
+        (data) => {
+          // console.log(data);  
+            
+          $("form").trigger("reset") //clear input after button was clicked
+        },
+        
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+
+      $.ajax({
+        url: `https://api.giphy.com/v1/gifs/translate?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+      }).then(
+        (data) => {
+          // console.log(data);  
+            
+          $("form").trigger("reset") //clear input after button was clicked
+        },
+        
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+})
+
+//will only be called in success path
+function getGif() {
+  
+}
+
+})
