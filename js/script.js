4534c21da16b0df91c53db61300dfc47712f379a
KZKS01 <kath.scholl@gmail.com>
2022-12-17 10:44:58 -0500

    WIP: all the buttons are working

--- js/script.js
+++ js/script.js
@@ -2,27 +2,27 @@
 $(document).ready(function() { 
   
     const $input1 = $("#box1");
+    const $input2 = $("#box2");
     
     
+    //trending gif
     $("#trending").on("click", function(evt){
 
         // alert("The button is clicked!");
         evt.preventDefault(); // prevents a page refresh on a 'click' event 
-        // console.log($input1.val());
-        var userInput1 = $input1.val();
-        
 
     $.ajax({
-        url: `https://api.giphy.com/v1/gifs/trending?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+        url: `https://api.giphy.com/v1/gifs/trending?&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
       }).then(
         (data) => {
-        console.log(data)
+        console.log(data) //to look at the array
          for(let i = 0; i < data.data.length; i++) {
           // alert("I am working!");
-            let gif = data.data[i].images.downsized_medium.url;
-            console.log(gif);
-            getGif(gif);
+          // console.log(gif); //to look at the links
+          let gif = data.data[i].images.downsized_medium.url;
+          getTrendingGif(gif);
         }
+      }),
         
         (error) => {
           console.log("bad request", error)
@@ -30,39 +30,74 @@ $(document).ready(function() {
     })
     
       //will only be called in success path
-      function getGif(gif) {
+      function getTrendingGif(gif) {
         $(".trendingGif").append(`<img src="${gif}">`);
       }
-    
-      // $.ajax({
-      //   url: `https://api.giphy.com/v1/gifs/search?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
-      // }).then(
-      //   (data) => {
-      //     // console.log(data);  
-            
-      //     $("form").trigger("reset") //clear input after button was clicked
-      //   },
-        
-      //   (error) => {
-      //     console.log("bad request", error)
-      //   }
-      // )
-
-      // $.ajax({
-      //   url: `https://api.giphy.com/v1/gifs/translate?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
-      // }).then(
-      //   (data) => {
-      //     // console.log(data);  
-            
-      //     $("form").trigger("reset") //clear input after button was clicked
-      //   },
-        
-      //   (error) => {
-      //     console.log("bad request", error)
-      //   }
-      // )
-})
-
 
       
-      })
+      //search gif
+      $("#search").on("click", function(evt){
+
+        evt.preventDefault(); // prevents a page refresh on a 'click' event 
+        var userInput1 = $input1.val();
+        // console.log($input1.val());
+
+      $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${userInput1}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+      }).then(
+        (data) => {
+        console.log(data); //to look at the array
+        for(let i = 0; i < data.data.length; i++) {
+          // alert("I am working!");
+            let gif = data.data[i].images.downsized_medium.url;
+            // console.log(gif); //to look at the links
+            getSearchedGif(gif);
+        }
+        $("form").trigger("reset") //clear input after button was clicked  
+        
+        },
+        
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+    })
+
+    function getSearchedGif(gif) {
+      $(".searchedGif").append(`<img src="${gif}">`);
+    }
+
+
+      //translate gif
+      $("#translate").on("click", function(evt){
+
+        evt.preventDefault(); // prevents a page refresh on a 'click' event 
+        var userInput2 = $input2.val();
+        // console.log($input1.val());
+
+      $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?q=${userInput2}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc&limit=5`,
+      }).then(
+        (data) => {
+        console.log(data); //to look at the array
+        for(let i = 0; i < data.data.length; i++) {
+          // alert("I am working!");
+            let gif = data.data[i].images.downsized_medium.url;
+            // console.log(gif); //to look at the links
+            getTranslatedGif(gif);
+        }
+        $("form").trigger("reset") //clear input after button was clicked  
+        
+        },
+        
+        (error) => {
+          console.log("bad request", error)
+        }
+      )
+    })
+
+    function getTranslatedGif(gif) {
+      $(".translatedGif").append(`<img src="${gif}">`);
+    }
+
+})
