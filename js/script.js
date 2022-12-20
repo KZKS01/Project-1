b39783b407e7c9a2fe156d78badb76be14961a24
KZKS01 <kath.scholl@gmail.com>
2022-12-19 21:46:47 -0500

    prepares for share button

--- js/script.js
+++ js/script.js
@@ -8,9 +8,16 @@ $(document).ready(function() {
     $("#img1").on("click", handleGetTrending);
 
     function handleGetTrending(evt){
-
       // alert("The button is clicked!");
       evt.preventDefault(); // prevents a page refresh on a 'click' event
+
+
+      function randomIndex (i){
+        var i = Math.floor(Math.random()*51); 
+        return i;
+      }
+      var valueOfI = randomIndex();
+
 
   $.ajax({
       url: `https://api.giphy.com/v1/gifs/trending?&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc`,
@@ -20,12 +27,11 @@ $(document).ready(function() {
       console.log(data) //to look at the array
       var i = Math.floor(Math.random()*51); 
       //console.log(i);
-      let gif = data.data[i].images.downsized_medium.url;
+      let gif = data.data[valueOfI].images.downsized_medium.url;
       //console.log(gif); //to look at the links
       getTrendingGif(gif);
       
     }),
-      
       (error) => {
         console.log("bad request", error)
       }
@@ -45,6 +51,13 @@ $(document).ready(function() {
         evt.preventDefault(); // prevents a page refresh on a 'click' event 
         var userInput = $input.val();
         // console.log($input.val());
+      
+      function randomIndex (i){
+        var i = Math.floor(Math.random()*51); 
+        return i;
+      }
+      var valueOfI = randomIndex();
+      // console.log(valueOfI);
 
       $.ajax({
         url: `https://api.giphy.com/v1/gifs/search?q=${userInput}&api_key=jR8VTUj3x87k9OpjfkvEu41hOtOz1MHc`,
@@ -53,10 +66,8 @@ $(document).ready(function() {
         // alert("I am working!");
         //console.log(data); //to look at the array
         //console.log(i);
-        var i = Math.floor(Math.random()*51); 
-        var gif = data.data[i].images.downsized_medium.url;
+        var gif = data.data[valueOfI].images.downsized_medium.url;
         getSearchedGif(gif);
-        return i;
         },
         
         (error) => {
@@ -67,8 +78,6 @@ $(document).ready(function() {
 
     function getSearchedGif(gif) {
       $("#img2").attr("src", gif);
-      var index =handleGetSearch(i)
-      console.log(index);
     }
 
     
